e07e66d73f0a0e9473fc4fee2eba5ab3531004c3
Matthieu Symoens <matthieu.symoens@gmail.com>
2024-12-28 19:29:00 -0500

    Updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import base from '@apeframework/eslint/base'
-import jest from '@apeframework/eslint/jest'
 import typescript from '@apeframework/eslint/typescript'
 
 const config = [
   base,
-  jest,
   typescript,
   {
     ignores: [
